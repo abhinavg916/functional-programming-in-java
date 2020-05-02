d2729d923a0fdeb1de1297d081940fcb94f58f1a
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-02 13:27:52 +0530

    Add Module 1

---
+++ Classwork/src/module1/FP00.java
@@ -0,0 +1,11 @@
+package module1;
+
+public class FP00 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, Java with Functional Programming");
+		System.out.println("Success setup of Java and Eclipse");
+
+	}
+
+}
