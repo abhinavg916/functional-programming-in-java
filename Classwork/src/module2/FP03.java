6ebd566db82e8cd47a2f875a78d12ac919c31220
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-02 13:40:57 +0530

    Add more code in Module 2

--- Classwork/src/module2/FP03.java
+++ Classwork/src/module2/FP03.java
@@ -1,6 +1,21 @@
-// Task - Print the squares of the numbers
+// Task - Print the squares of even numbers in the list
 package module2;
 
-public class FP03 {
+import java.util.List;
 
+public class FP03 {
+	public static void main(String[] args) {
+
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		printSquaresofEvenNumbersInListFunctional(numbers);
+	}
+
+	private static void printSquaresofEvenNumbersInListFunctional(List<Integer> numbers) {
+
+		numbers.stream()
+		.filter(number -> number % 2 == 0) // Use of Lambda Expression
+		.map(x -> x*x) // Use of map for mapping x -> x*x
+		.forEach(System.out::println);
+
+	}
 }
