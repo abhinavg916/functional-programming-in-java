800ed18c08aa9c904cb0a0e0959fce7687c95d82
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-03 10:33:36 +0530

    Use of reduce using JShell

---
+++ Classwork/src/module3/FP05.java
@@ -0,0 +1,42 @@
+//Task - Find the maximum and minimum number from the list
+package module3;
+
+public class FP05 {
+
+	public static void main(String[] args) {
+//			 JShell
+//		|  Welcome to JShell -- Version 13.0.1
+//		|  For an introduction type: /help intro
+//
+//		jshell> System.out.println("Hello World in JShell by Abhinav")
+//		Hello World in JShell by Abhinav
+//
+//		jshell> System.out.println("Hello World in JShell by Abhinav");
+//		Hello World in JShell by Abhinav
+//
+//		jshell> List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15)
+//		numbers ==> [12, 9, 13, 4, 6, 2, 4, 12, 15]
+//
+//		jshell> numbers.stream().reduce(0, (a,b) -> a+b)
+//		$4 ==> 77
+//
+//		jshell> numbers.stream().reduce(0, (a,b) -> a)
+//		$5 ==> 0
+//
+//		jshell> numbers.stream().reduce(0, (a,b) -> a>b?a:b)
+//		$6 ==> 15
+//		
+//		jshell> numbers.stream().reduce(Integer.MIN_VALUE, (a,b) -> a>b?a:b)
+//		$8 ==> 15
+//		
+//		jshell> numbers.stream().reduce(Integer.MAX_VALUE, (a,b) -> a>b?b:a)
+//		$10 ==> 2
+//		
+//		jshell> numbers.stream().reduce(Integer.MIN_VALUE, (a,b) -> a<b?a:b)
+//		$8 ==> -2147483648
+//		
+//		jshell> numbers
+//		numbers ==> [12, 9, 13, 4, 6, 2, 4, 12, 15]
+	}
+
+}
