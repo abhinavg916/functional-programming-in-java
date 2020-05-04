50bb56333108270814da7932bc48173415e86fee
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-04 16:15:35 +0530

    File Rename

--- Classwork/src/module3/FP10.java
+++ Classwork/src/module3/FP10.java
@@ -5,26 +5,26 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class F10 {
+public class FP10 {
 
 	public static void main(String[] args) {
 		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 		
 		numbers.stream()
-			.distinct() //Stream<T> Intermediate
-			.sorted() //Stream<T>
-			.forEach(System.out::println); //void
+			.distinct() // Returns Stream<T> Intermediate
+			.sorted() // Returns Stream<T>
+			.forEach(System.out::println); // Returns void
 
 		List<Integer> squaredNumbers = numbers.stream()
-				.map(number -> number * number) //Stream<R>
-				.collect(Collectors.toList()); //R
+				.map(number -> number * number) // Returns Stream<R>
+				.collect(Collectors.toList()); // Returns R
 
 		List<Integer> evenNumbersOnly = numbers.stream()
-				.filter(x -> x % 2 == 0) //Stream<T>
+				.filter(x -> x % 2 == 0) //Returns Stream<T>
 				.collect(Collectors.toList());
 		
 		int sum = numbers.stream()
-				.reduce(0, (x,y) -> x*x + y*y); //T
+				.reduce(0, (x,y) -> x*x + y*y); //Returns T
 		
 		int greatest = numbers.stream()
 				.reduce(Integer.MIN_VALUE, (x,y)-> x>y ? x:y);
--- Homework/src/module3/Exercise10.java
+++ Homework/src/module3/Exercise10.java
@@ -1,14 +1,17 @@
 package module3;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Exercise10 {
 
 	public static void main(String[] args) {
+
 		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
 		List<Integer> evenNumbers = evenList(numbers);
 		System.out.println(evenNumbers);
+		List<Integer> evenNumbers1 = numbers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
 	}
 
 	private static List<Integer> evenList(List<Integer> numbers) {
