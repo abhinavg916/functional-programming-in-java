282e43f173ab9ff98d476b9bb4c5bbe19797673b
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-05 17:43:18 +0530

    Exercise 11 Errors Resolved

--- Homework/src/module3/Exercise11.java
+++ Homework/src/module3/Exercise11.java
@@ -1,22 +1,22 @@
 package module3;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Exercise11 {
 
+
 	public static void main(String[] args) {
 		List<String> courses = List.of("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure", "Docker",
 				"Kubernetes");
-		List<String> courselen = courseLenList(courses);
+		List<Integer> courselen = courseLenList(courses);
 		System.out.println(courselen);
 	}
 
-	private static List<String> courseLenList(List<String> courses) {
+	private static List<Integer> courseLenList (List<String> courses) {
 		return courses.stream().map(str -> str.length()).collect(Collectors.toList());
 	}
 //		jshell>courses.stream().map(str -> str.length()).collect(Collectors.toList());
 //		$19 ==> [6, 11, 3, 13, 3, 3, 5, 6, 10]
-	
+
 }
