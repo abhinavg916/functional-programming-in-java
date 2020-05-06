35de1aed73b3b60dff1dca0fe4812a06cc44368f
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-06 09:07:45 +0530

    Add Exercise 13

--- Solutions/src/module4/Exercise13.java
+++ Solutions/src/module4/Exercise13.java
@@ -1,9 +1,28 @@
 package module4;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class Exercise13 {
 
 	public static void main(String[] args) {
+//		Given, List squaredNumbers = numbers.stream().map(x -> x*x).collect(Collectors.toList());
+		List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		List squaredNumbers = numbers.stream().map(x -> x * x).collect(Collectors.toList());
 
+		// Using Behvaior Parameterisation
+		List squaredNumbers2 = mapAndCreateNewList(numbers, x -> x * x);
+		List cubedNumbers = mapAndCreateNewList(numbers, x -> x * x * x);
+		List doubledNumbers = mapAndCreateNewList(numbers, x -> 2 * x);
+		System.out.println(squaredNumbers2);
+		System.out.println(cubedNumbers);
+		System.out.println(doubledNumbers);
+	}
+
+	private static List<? extends Integer> mapAndCreateNewList(List<Integer> numbers,
+			Function<Integer, Integer> mappingFunction) {
+		return numbers.stream().map(mappingFunction).collect(Collectors.toList());
 	}
 
 }
