18ee922ce9e5fafd37c780013d51f669268c470b
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-12 19:54:30 +0530

    Add Exercise of Module 6

---
+++ Solutions/src/module6/Exercise.java
@@ -0,0 +1,22 @@
+package module6;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+
+//		jshell> IntStream.iterate(0, e -> e+2).limit(10).peek(System.out::println).sum()
+//		0
+//		2
+//		4
+//		6
+//		8
+//		10
+//		12
+//		14
+//		16
+//		18
+//		$4 ==> 90
+
+	}
+
+}
