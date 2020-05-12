9c9e641ba9651169b689459ed2e98d489b30ce61
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-12 19:54:04 +0530

    Add iterate()

--- Lectures/src/module6/FP22CreationofStreamusingMethodsandforArrays.java
+++ Lectures/src/module6/FP22CreationofStreamusingMethodsandforArrays.java
@@ -49,6 +49,74 @@ public class FP22CreationofStreamusingMethodsandforArrays {
 //		
 //		jshell> IntStream.range(1,10).sum()
 //		$10 ==> 45
+		
+//		jshell> IntStream.iterate(1, e -> e+2).limit(10).peek(System.out::println).sum()
+//		1
+//		3
+//		5
+//		7
+//		9
+//		11
+//		13
+//		15
+//		17
+//		19
+//		$3 ==> 100
+//
+//		jshell> IntStream.iterate(0, e -> e+2).limit(10).peek(System.out::println).sum()
+//		0
+//		2
+//		4
+//		6
+//		8
+//		10
+//		12
+//		14
+//		16
+//		18
+//		$4 ==> 90
+//
+//		jshell> IntStream.iterate(2, e -> e*2).limit(10).peek(System.out::println).sum()
+//		2
+//		4
+//		8
+//		16
+//		32
+//		64
+//		128
+//		256
+//		512
+//		1024
+//		$5 ==> 2046
+//		
+//		jshell> IntStream.iterate(2, e -> e*2).limit(10).peek(System.out::println).sum()
+//		2
+//		4
+//		8
+//		16
+//		32
+//		64
+//		128
+//		256
+//		512
+//		1024
+//		$5 ==> 2046
+//
+//		jshell> IntStream.iterate(2, e -> e*2).limit(10).collect(Collectors.toList())
+//		|  Error:
+//		|  method collect in interface java.util.stream.IntStream cannot be applied to given types;
+//		|    required: java.util.function.Supplier<R>,java.util.function.ObjIntConsumer<R>,java.util.function.BiConsumer<R,R>
+//		|    found:    java.util.stream.Collector<java.lang.Object,capture#2 of ?,java.util.List<java.lang.Object>>
+//		|    reason: cannot infer type-variable(s) R
+//		|      (actual and formal argument lists differ in length)
+//		|  IntStream.iterate(2, e -> e*2).limit(10).collect(Collectors.toList())
+//		|  ^----------------------------------------------^
+//
+//		jshell> IntStream.iterate(2, e -> e*2).limit(10).boxed().collect(Collectors.toList())
+//		$6 ==> [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+		
+		
+
 
 	}
 
