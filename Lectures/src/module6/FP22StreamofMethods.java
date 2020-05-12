4485f84a71989e599527eed1064907cd4989bf2b
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-12 12:56:52 +0530

    More on Stream using Method and for Arrays

--- Lectures/src/module6/FP22StreamofMethods.java
+++ Lectures/src/module6/FP22StreamofMethods.java
@@ -4,6 +4,46 @@ public class FP22StreamofMethods {
 
 	public static void main(String[] args) {
 		
+//		jshell> List<Integer> numbers = List.of(12,9,13,4,6,2,4,12,15);
+//		numbers ==> [12, 9, 13, 4, 6, 2, 4, 12, 15]
+//
+//		jshell> numbers.stream()
+//		$2 ==> java.util.stream.ReferencePipeline$Head@14bf9759
+//
+//		jshell> Stream.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+//		$3 ==> java.util.stream.ReferencePipeline$Head@553f17c
+//
+//		jshell> Stream.of(12, 9, 13, 4, 6, 2, 4, 12, 15).count()
+//		$4 ==> 9
+//
+//		jshell> Stream.of(12, 9, 13, 4, 6, 2, 4, 12, 15).reduce(0, Integer::sum)
+//		$5 ==> 77
+//		
+//		jshell> Stream.of(12, 9, 13, 4, 6, 2, 4, 12, 15).average() // Not supported
+//		|  Error:
+//			|  cannot find symbol
+//			|    symbol:   method average()
+//			|  Stream.of(12, 9, 13, 4, 6, 2, 4, 12, 15).average()
+//			|  ^----------------------------------------------^
+//			
+//		jshell> int[] numberArray = {12, 9, 13, 4, 6, 2, 4, 12, 15}
+//		numberArray ==> int[9] { 12, 9, 13, 4, 6, 2, 4, 12, 15 }
+//		
+//		jshell> Arrays.stream(numberArray)
+//		$7 ==> java.util.stream.IntPipeline$Head@1ce92674
+//		
+//		jshell> Arrays.stream(numberArray).sum()
+//		$3 ==> 77
+//
+//		jshell> Arrays.stream(numberArray).average()
+//		$4 ==> OptionalDouble[8.555555555555555]
+//
+//		jshell> Arrays.stream(numberArray).min()
+//		$5 ==> OptionalInt[2]
+//
+//		jshell> Arrays.stream(numberArray).max()
+//		$6 ==> OptionalInt[15]
+
 	}
 
 }
