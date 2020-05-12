47ba9abba13439062f33cd35f51dd8eadca4ac1d
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-12 19:54:51 +0530

    Add Big Number Calculations with Big Integer for larger values

---
+++ Lectures/src/module6/FP23BigNumberCalculationswithBigInteger.java
@@ -0,0 +1,25 @@
+package module6;
+
+public class FP23BigNumberCalculationswithBigInteger {
+
+	public static void main(String[] args) {
+//		jshell> Integer.MAX_VALUE
+//		$7 ==> 2147483647
+//
+//		jshell> Long.MAX_VALUE
+//		$8 ==> 9223372036854775807
+//
+//		jshell> IntStream.rangeClosed(1,50).reduce(1, (x,y) -> x*y)
+//		$9 ==> 0
+//
+//		jshell> LongStream.rangeClosed(1,50).reduce(1, (x,y) -> x*y)
+//		$10 ==> -3258495067890909184
+//
+//		jshell> LongStream.rangeClosed(1,20).reduce(1, (x,y) -> x*y)
+//		$11 ==> 2432902008176640000
+//
+//		jshell> LongStream.rangeClosed(1,50).mapToObj(BigInteger::valueOf).reduce(BigInteger.ONE, BigInteger::multiply)
+//		$12 ==> 30414093201713378043612608166064768844377641568960512000000000000
+	}
+
+}
