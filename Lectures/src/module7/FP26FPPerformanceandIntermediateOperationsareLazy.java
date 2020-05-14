82d82783765f1146856dfbaa464f1d4eba0dca2b
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-14 22:52:42 +0530

    Add FP Performance and Intermediate Operations

---
+++ Lectures/src/module7/FP26FPPerformanceandIntermediateOperationsareLazy.java
@@ -0,0 +1,29 @@
+package module7;
+
+public class FP26FPPerformanceandIntermediateOperationsareLazy {
+	public static void main(String[] args) {
+		
+//		jshell> courses.stream().filter(course -> course.length()>11).map(String::toUpperCase).findFirst()
+//		$10 ==> Optional[MICROSERVICES]
+		
+//		jshell> courses.stream().peek(System.out::println).filter(course -> course.length()>11).map(String::toUpperCase).peek(System.out::println).findFirst()
+//		Spring
+//		Spring Boot
+//		API
+//		Microservices
+//		MICROSERVICES
+//		$12 ==> Optional[MICROSERVICES]
+		
+//		jshell> courses.stream().peek(System.out::println).filter(course -> course.length()>11).map(String::toUpperCase).peek(System.out::println)
+//		$13 ==> java.util.stream.ReferencePipeline$11@148080bb // $13 is a variable that is made by JShell
+//		jshell> $13.findFirst() // Now, statement is executed and returns the result
+//		Spring
+//		Spring Boot
+//		API
+//		Microservices
+//		MICROSERVICES
+//		$14 ==> Optional[MICROSERVICES]
+
+
+	}
+}
